9c862f6ff85e04a3ef65f96a7fc051aae9bb952b
Quytelda Kahja <quytelda@tamalin.org>
2015-03-30 21:08:44 -0500

    Add vegetation ranges.

--- src/land.h
+++ src/land.h
@@ -6,7 +6,6 @@
 #define NUM_PEAKS 128
 #define PLANT_VALUE	100
 
-
 // Probabilities of landscape features.
 // Probabilities are integers between 0 and 100, inclusive.
 #define PROB_SOIL_FERTILITY 20 // probability a soil square will have plants
@@ -17,11 +16,30 @@
 
 typedef enum {ROCK, SOIL, SAND, WATER} terrain_t;
 
+typedef struct range { char start, end; } range;
+
+static const range PLANT_D = {1, 9};
+static const range FLOWERS_D = {2, 8};
+static const range ROOTS_D = {3, 6};
+static const range FRUITS_D = {2, 7};
+static const range SEEDS_D = {3, 8};
+
+static const range PLANT_F = {1, 10};
+static const range FLOWERS_F = {3, 10};
+static const range ROOTS_F = {3, 10};
+static const range FRUITS_F = {5, 10};
+static const range SEEDS_F = {3, 10};
+
+static const range PLANT_M = {2, 9};
+static const range FLOWERS_M = {3, 7};
+static const range ROOTS_M = {4, 8};
+static const range FRUITS_M = {2, 8};
+static const range SEEDS_M = {4, 6};
+
 #define M_FRUIT  0
 #define M_FLOWER 1
 #define M_ROOT   2
 #define M_SEED   3
-#define M_STEM   4
 
 typedef struct plant
 {
