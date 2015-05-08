72432c1e0e700e0f35a9b6e3adcac5bb5b5d7a92
Quytelda Kahja <quytelda@tamalin.org>
2015-05-08 14:59:21 -0500

    Start land file.

--- src/land.c
+++ src/land.c
@@ -1,1 +1,29 @@
+#include "land.h"
 
+land_t init_land(world_t * world)
+{
+	land_t ret;
+}
+
+void init_world(world_t * world)
+{
+	world->land = malloc(world->rows * sizeof(land_t *));
+	for(int i = 0; i < world->rows; i++)
+	{
+		world->land[i] = malloc(world->cols * sizeof(land_t));
+
+		// set default values
+		for(int j = 0; j < world->cols; j++)
+		{
+			world->land[i][j] = init_land();
+		}
+	}
+}
+
+/**
+ * "Now, I am become death, the Destroyer of Worlds"
+ * "Kora li hiro, vankelda quinar."
+ */
+void destroy_world(world_t * world)
+{
+}
--- src/land.h
+++ src/land.h
@@ -1,1 +1,29 @@
+#ifndef __LAND_H
+#define __LAND_H
 
+/* `land_t` represents one square unit of land */
+typedef struct land
+{
+	short density;
+	short fertility;
+	short moisture;
+
+	// TODO: plant list
+	// TODO: animal list
+} land_t;
+
+typedef struct world
+{
+	land_t ** land;
+	size_t rows, cols;
+
+	// global biome disposition
+	short density;
+	short fertility;
+	short moisture;
+} world_t;
+
+void init_world(world_t * world);
+void destroy_world(world_t * world);
+
+#endif
